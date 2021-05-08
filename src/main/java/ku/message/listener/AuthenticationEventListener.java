f278a746874018f0575130a59c2881d05d30a55a
Ing <ingkharat.j@ku.th>
2021-05-08 23:10:57 +0700

    finished

---
+++ src/main/java/ku/message/listener/AuthenticationEventListener.java
@@ -0,0 +1,33 @@
+package ku.message.listener;
+
+
+import ku.message.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationEventListener {
+
+    Logger logger = LoggerFactory.getLogger(AuthenticationEventListener.class);
+
+    @Autowired
+    private UserService userService;
+
+    @EventListener
+    //AuthenticationFailureBadCredentialsEvent is for bad username and pwd, and can to change to anything.
+    public void authenticationFailed(AuthenticationFailureBadCredentialsEvent event) {
+
+        String username = (String) event.getAuthentication().getPrincipal();
+
+        if (userService.isUsernameAvailable(username))
+            logger.warn("Failed login attempt [incorrect USERNAME]");
+        else
+            logger.warn("Failed login attempt [incorrect PASSWORD]");
+    }
+
+
+}
